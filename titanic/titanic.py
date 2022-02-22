4b3a72332bd68f8bef9a2a32971c4697b006ef87
John <johnbt@runbox.com>
2022-02-22 15:54:53 -0700

    init

---
+++ titanic/titanic.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+titanic = pd.read_csv("https://raw.githubusercontent.com/pandas-dev/pandas/master/doc/data/titanic.csv")
+
+def mean(field="Age"):
+    return float(titanic[field].mean())
+
+def median(field="Age"):
+    return float(titanic[field].median())
+
+def percentile(field="Age", percentile=.25):
+    return float(titanic[field].quantile(float(percentile)))
+
+def raw(rows: int=5):
+    return titanic.head(int(rows))

5df76bde85706d8bc3c3278165d3af301150f1c9
John <johnbt@runbox.com>
2022-02-22 19:56:31 -0700

    update outputs

--- json_to_df/json_out_to_df.py
+++ json_to_df/json_out_to_df.py
@@ -2,7 +2,7 @@ import pandas as pd
 import pickle
 
 def convert(json_out, field = "title"):
-    data = [o[field] for o in json_out[0]['result']]
+    data = [o[field] for o in json_out]
     df = [{"id": i, "description": t} for i, t in enumerate(data)]
 
     return df

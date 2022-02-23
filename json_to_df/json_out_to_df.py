cfd3c5451b418aaa0595e1e9684b038739ec6867
John <johnbt@runbox.com>
2022-02-22 17:33:00 -0700

    json

--- json_to_df/json_out_to_df.py
+++ json_to_df/json_out_to_df.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def convert(json_out, field = "title"):
-    data = [o[field] for i in json_out[0]]
+    data = [o[field] for o in json_out[0]]
     df = pd.DataFrame([{"id": i, "description": t} for i, t in enumerate(data)])
 
     return df

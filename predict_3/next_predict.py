01e5b31f3a91a0f05bc30cc85f9867210853ef31
John <johnbt@runbox.com>
2022-04-07 16:12:19 -0700

    update

---
+++ predict_3/next_predict.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def estimate_hires(df:pd.DataFrame):
+    values = df['trend'].values
+    hires = (values / 20000) + 2
+
+    dates = df['ds'].values
+
+    new_df = pd.DataFrame()
+    new_df['date'] = dates
+    new_df['New hires count'] = hires
+
+    return new_df

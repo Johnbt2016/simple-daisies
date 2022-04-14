77782f50935b319b209db69a2b724aca9d641eee
John <johnbt@runbox.com>
2022-04-14 08:53:46 -0600

    update

--- ImageColor/img_color.py
+++ ImageColor/img_color.py
@@ -8,7 +8,7 @@ sd = SharedDataClient(access_token="iYDE89fkV6iQhRQdhJYn6q6QMiJy1tW3")
 
 def give_color(repeat = 1):
     results = []
-    for i in range(repeat):
+    for i in range(int(repeat)):
         obj = sd.download_fileobj("/JM/Images/B2DBy.jpeg")
         img = io.BytesIO(obj)
         results.append(color.run_(img))

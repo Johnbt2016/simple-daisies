7789d7c8d02918cfb4bb4ebc2576e44535f19841
John <johnbt@runbox.com>
2022-04-14 14:10:20 -0600

    update

---
+++ ETL/etl.py
@@ -0,0 +1,36 @@
+import json
+
+def extract():
+    """
+    #### Extract task
+    A simple Extract task to get data ready for the rest of the data
+    pipeline. In this case, getting data is simulated by reading from a
+    hardcoded JSON string.
+    """
+    data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+
+    order_data_dict = json.loads(data_string)
+    return order_data_dict
+
+
+def transform(order_data_dict: dict):
+    """
+    #### Transform task
+    A simple Transform task which takes in the collection of order data and
+    computes the total order value.
+    """
+    total_order_value = 0
+
+    for value in order_data_dict.values():
+        total_order_value += value
+
+    return {"total_order_value": total_order_value}
+
+def load(total_order_value: float):
+    """
+    #### Load task
+    A simple Load task which takes in the result of the Transform task and
+    instead of saving it to end user review, just prints it out.
+    """
+
+    return "Total order value is:" + str(total_order_value)

b75844c4cc661f4777a3d772a9f527bee321e818
John <johnbt@runbox.com>
2022-04-25 13:21:28 -0600

    streamlit

--- streamlit_example/ex2.py
+++ streamlit_example/ex2.py
@@ -3,10 +3,8 @@ import streamlit as st
 def hello(name):
     return "Hello " + str(name)
 
-if __name__ == "__main__":
 
+st.title("Hello World Demo")
+name = st.text_area(label="Your Name", value="World")
 
-    st.title("Hello World Demo")
-    name = st.text_area(label="Your Name", value="World")
-
-    st.write(hello(name=name))
+st.write(hello(name=name))

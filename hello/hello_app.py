d78ee0f9733fd9c73b377d07964c15066f5c2404
John <johnbt@runbox.com>
2022-05-04 10:26:05 -0600

    update

--- hello/hello_app.py
+++ hello/hello_app.py
@@ -3,8 +3,10 @@ import streamlit as st
 def hello(name="World"):
   return "Hello " + str(name)
 
-name = st.text_input('Type your name')
+def st_ui():
+  name = st.text_input('Type your name')
+  greeting = hello(name)
+  st.write(greeting)
 
-greeting = hello(name)
-
-st.write(greeting)
+if __name__ == "__main__":
+  st_ui()

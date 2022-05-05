467a92beee2d5e516e1209590a68853a3b79e792
John <johnbt@runbox.com>
2022-05-05 13:02:43 -0600

    update

--- classify_list/classify_list.py
+++ classify_list/classify_list.py
@@ -4,6 +4,7 @@ import pandas as pd
 classify = pyd.Daisi("Zero Shot Text Classification")
 
 def get_labels(df, column, candidate_labels):
+    print(df)
     data_list = df[column].to_list()
     labels = [classify.compute(text = n, candidate_labels = candidate_labels) for n in data_list]
     return labels

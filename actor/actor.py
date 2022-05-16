b8eb0893e8ae8c99ceb15a8e55a77a3e6fc7503a
John <johnbt@runbox.com>
2022-05-16 15:40:19 -0600

    update

--- actor/actor.py
+++ actor/actor.py
@@ -1,15 +1,15 @@
 class ActorExample:
 
     def __init__(self):
-        self.param1 = None
-        self.param2 = None
+        self.param1 = 0
+        self.param2 = 0
 
 def get(actor):
     return actor
 
 def update(actor, param1=None, param2=None):
     if param1 is not None:
-        actor.param1 = param1
+        actor.param1 += param1
     if param2 is not None:
-        actor.param2 = param2
+        actor.param2 += param2
     return actor

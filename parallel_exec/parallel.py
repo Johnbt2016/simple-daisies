5fb8de9f03c9c1c9933354ddd0bb72e950fc62a6
John <johnbt@runbox.com>
2022-05-19 10:06:22 -0600

    update

--- parallel_exec/parallel.py
+++ parallel_exec/parallel.py
@@ -1,16 +1,19 @@
-from pydaisi import Daisi
+import numpy as np
 import time
 
-# start = time.time()
-# md = Daisi("GoogleNews")
-# print(md.get_news(query="Apple").value)
-# print(time.time() - start)
 
+def apply_func(data_point, size=10000):
+    curve_x = np.arange(size) 
+    curve_y = 100*np.random.rand(size,)
 
-start = time.time()
-with Daisi("GoogleNews") as my_daisi:
-    calls = [my_daisi.get_news_(query="Apple") for _ in range(100)]
+    res = np.interp(data_point, curve_x, curve_y)
 
-    print(Daisi.run_parallel(*calls))
+    return res
 
-print(time.time() - start)
+if __name__ == "__main__":
+    data_size = 100000
+    data = 100 * np.random.rand(data_size,)
+    start = time.time()
+    res = np.array([apply_func(d) for d in data])
+    print(time.time() - start)
+    # print(res)

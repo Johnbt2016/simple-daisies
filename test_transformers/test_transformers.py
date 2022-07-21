c83077108e2fccbc6dfa562c749e876f95989742
John <johnbt@runbox.com>
2022-07-21 11:03:20 -0600

    update

---
+++ test_transformers/test_transformers.py
@@ -0,0 +1,28 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
+model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
+
+def generate_summary(text, min_length = 80, max_length=150):
+    """
+    Generate a summarized version of given text.
+    
+    Parameters
+    ----------
+    min_length: int
+        The minimum word count of the summary
+    max_length: int
+        The maximum word count of the summary  
+
+    Returns
+    -------
+    str
+    """
+    inputs = tokenizer.encode("summarize: " + text,
+            return_tensors='pt',
+            max_length=512,
+            truncation=True
+        )
+    summary_ids = model.generate(inputs, max_length=max_length, min_length=min_length, length_penalty=5., num_beams=2)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return summary

23d5bd16a1f9be2f03117c327e7e2621c14bec47
John <johnbt@runbox.com>
2022-07-20 21:04:33 -0600

    update

--- fig_test/fig_test.py
+++ fig_test/fig_test.py
@@ -1,8 +1,4 @@
-import matplotlib
 import matplotlib.pyplot as plt
-
-
-
 
 def get_fig():
     a = [0,1]
@@ -10,26 +6,3 @@ def get_fig():
     ax.plot(a, a)
 
     return fig
-
-def show_figure(fig):
-
-    # create a dummy figure and use its
-    # manager to display "fig"  
-    dummy = plt.figure()
-    new_manager = dummy.canvas.manager
-    new_manager.canvas.figure = fig
-    fig.set_canvas(new_manager.canvas)
-
-    return fig
-
-
-
-
-if __name__ == "__main__":
-    fig = get_fig()
-
-    dummy = plt.figure()
-    new_manager = dummy.canvas.manager
-    new_manager.canvas.figure = fig
-    fig.set_canvas(new_manager.canvas)
-    fig.savefig("ttt.png")

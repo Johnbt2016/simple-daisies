e1e5306cc08e22fc7d84c4a68da302562dc9804d
John <johnbt@runbox.com>
2022-07-21 11:21:16 -0600

    update

--- test_transformers/test_transformers.py
+++ test_transformers/test_transformers.py
@@ -1,30 +1,22 @@
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import torch
 import sentencepiece
+from transformers import pipeline
 
-tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
-model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
+# tokenizer = AutoTokenizer.from_pretrained("sshleifer/distilbart-cnn-12-6")
+# model = AutoModelForSeq2SeqLM.from_pretrained("sshleifer/distilbart-cnn-12-6")
 
 def generate_summary(text, min_length = 80, max_length=150):
-    """
-    Generate a summarized version of given text.
     
-    Parameters
-    ----------
-    min_length: int
-        The minimum word count of the summary
-    max_length: int
-        The maximum word count of the summary  
+    summarizer = pipeline("summarization", model="philschmid/distilbart-cnn-12-6-samsum")
 
-    Returns
-    -------
-    str
-    """
-    inputs = tokenizer.encode("summarize: " + text,
-            return_tensors='pt',
-            max_length=512,
-            truncation=True
-        )
-    summary_ids = model.generate(inputs, max_length=max_length, min_length=min_length, length_penalty=5., num_beams=2)
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    return summary
+    conversation = '''Jeff: Can I train a 🤗 Transformers model on Amazon SageMaker? 
+    Philipp: Sure you can use the new Hugging Face Deep Learning Container. 
+    Jeff: ok.
+    Jeff: and how can I get started? 
+    Jeff: where can I find documentation? 
+    Philipp: ok, ok you can find everything here. https://huggingface.co/blog/the-partnership-amazon-sagemaker-and-hugging-face                                           
+    '''
+    out = summarizer(conversation)
+
+    return out

8d0c9c00caeddfaced42879259e8772ae5cf6dda
John <johnbt@runbox.com>
2022-09-22 15:51:13 -0600

    update

---
+++ sys_cmd/sys_cmd.py
@@ -0,0 +1,20 @@
+import os
+import streamlit as st
+
+
+def exec_cmd(prompt):
+    return os.popen(prompt).read()
+
+
+def st_ui():
+    st.title("Daisi platform system level commands")
+
+    prompt = st.text_area("Enter your command here")
+
+    if st.button("Execute"):
+        st.write(exec_cmd(prompt))
+
+if __name__ == "__main__":
+    prompt = 'rm -rf test.py'
+
+    print(exec_cmd(prompt))

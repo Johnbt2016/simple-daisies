83ef2b29bc7da30bb71f28496c016e05567a4907
John <johnbt@runbox.com>
2022-09-22 19:59:05 -0600

    update

--- sys_cmd/sys_cmd.py
+++ sys_cmd/sys_cmd.py
@@ -2,15 +2,12 @@ import os
 import streamlit as st
 
 
-def exec_cmd(prompt, use_streamlit = False):
-    if not use_streamlit:
-        output = []
-        
-        for line in os.popen(prompt).readlines():
-            output.append(line)
-        return output
-    else:
-        return os.popen(prompt)
+def exec_cmd(prompt):
+    res = os.popen(prompt)
+    output = []
+    for line in res.readlines():
+        output.append(line)
+    return output
 
 def st_ui():
     st.title("Daisi platform system level commands")
@@ -19,10 +16,9 @@ def st_ui():
 
     if st.button("Execute"):
         
-        res = exec_cmd(prompt, use_streamlit = True)
-        for line in res.readlines():
+        res = exec_cmd(prompt)
+        for line in res:
             st.write(line)
-        print(exec_cmd(prompt).read())
 
 if __name__ == "__main__":
     st_ui()
